a4e4532c08d4ed5da5680422fcb9ef2e3efc9923
Jerrin Shaji George <jshajigeorge@vmware.com>
2023-05-11 13:38:59 -0700

    Update build script to install dependecies

--- build.sh
+++ build.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 set -x
+curDir=`pwd`
+cd /tmp
+wget https://raw.githubusercontent.com/vmware-tanzu/carvel-kapp-controller/83fffcfe99a65031b4170813acf94f8d5058b346/hack/dependencies.yml
+wget https://raw.githubusercontent.com/vmware-tanzu/carvel-kapp-controller/83fffcfe99a65031b4170813acf94f8d5058b346/hack/install-deps.sh
+chmod a+x ./install-deps.sh
+./install-deps.sh
+cd $curDir
 echo "# sieve.client v0.0.0
 ## explicit; go 1.19
 sieve.client" >> vendor/modules.txt
