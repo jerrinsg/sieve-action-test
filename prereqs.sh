9bae382dad23f102d817754e1098121af9cacdfb
Jerrin Shaji George <jshajigeorge@vmware.com>
2023-05-11 15:40:32 -0700

    Install coreutils
    In order to work around error:
    Missing sha256sum binary

--- prereqs.sh
+++ prereqs.sh
@@ -1,4 +1,5 @@
 !/bin/bash
+brew install coreutils
 curDir=`pwd`
 cd /tmp
 wget https://raw.githubusercontent.com/vmware-tanzu/carvel-kapp-controller/83fffcfe99a65031b4170813acf94f8d5058b346/hack/dependencies.yml
