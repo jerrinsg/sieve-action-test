fc00dc0fc568631086ae6d01361bed75e032b94c
Jerrin Shaji George <jshajigeorge@vmware.com>
2023-05-18 21:48:18 -0700

    Create prereqs-linux.sh

---
+++ prereqs-linux.sh
@@ -0,0 +1,9 @@
+!/bin/bash
+sudo apt-get install -y coreutils
+curDir=`pwd`
+cd /tmp
+wget https://raw.githubusercontent.com/vmware-tanzu/carvel-kapp-controller/83fffcfe99a65031b4170813acf94f8d5058b346/hack/dependencies.yml
+wget https://raw.githubusercontent.com/vmware-tanzu/carvel-kapp-controller/83fffcfe99a65031b4170813acf94f8d5058b346/hack/install-deps.sh
+chmod a+x ./install-deps.sh
+./install-deps.sh
+cd $curDir
